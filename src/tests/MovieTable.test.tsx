0b89728d19a3a18219094473ae97d6fde3850062
YagoGodoyGarcia <yagogodoygarcia@outlook.com>
2025-06-28 01:11:38 -0300

    Teste renderizacao MovieTable

--- src/tests/MovieTable.test.tsx
+++ src/tests/MovieTable.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { screen, render } from './test-utils'; // usa o custom render com BrowserRouter
 import MovieTable from '../components/MovieList/MovieTable';
 
 const mockMovies = [
@@ -15,6 +15,14 @@ const mockMovies = [
 describe('MovieTable', () => {
   it('exibe os dados dos filmes corretamente', () => {
     render(<MovieTable movies={mockMovies} />);
+
+    // Aguarda os títulos aparecerem 
+    expect(screen.findByText('Título')).toBeInTheDocument();
+    expect(screen.findByText('Ano')).toBeInTheDocument();
+    expect(screen.findByText('Estúdio')).toBeInTheDocument();
+    expect(screen.findByText('Produtores')).toBeInTheDocument();
+    expect(screen.findByText('Vencedor?')).toBeInTheDocument();
+    // Verifica dados renderizados
     expect(screen.getByText('Filme A')).toBeInTheDocument();
     expect(screen.getByText('1990')).toBeInTheDocument();
     expect(screen.getByText('Sim')).toBeInTheDocument();
--- src/tests/TopStudiosTable.test.tsx
+++ src/tests/TopStudiosTable.test.tsx
@@ -15,14 +15,12 @@ describe('TopStudiosTable', () => {
   it('exibe os estúdios com mais vitórias', async () => {
     render(<TopStudiosTable />);
 
-
     // Aguarda os títulos aparecerem 
     expect(await screen.findByText(/Top 3 estúdios com mais vitórias/i)).toBeInTheDocument();
     expect(await screen.findByText('Estúdio')).toBeInTheDocument();
     expect(await screen.findByText('Vitórias')).toBeInTheDocument();
 
     await waitFor(() => {
-
       // Verifica dados renderizados
       expect(screen.getByText('Studio A')).toBeInTheDocument();
       expect(screen.getByText(5)).toBeInTheDocument();
