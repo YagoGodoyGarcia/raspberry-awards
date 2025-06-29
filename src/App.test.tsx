3730219e9e41e85896a0d526fa79886408bfd769
YagoGodoyGarcia <yagogodoygarcia@outlook.com>
2025-06-29 16:13:20 -0300

    ajuste arquivo de teste para componente app

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,7 +1,11 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
+describe('App', () => {
+  it('renderiza o layout com link para o Dashboard', () => {
+    render(<App />);
+
+    const dashboardLink = screen.getByRole('link', { name: /dashboard/i });
+    expect(dashboardLink).toBeInTheDocument();
+  });
 });
